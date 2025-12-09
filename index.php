1eb56cf0fee75b192bd99c63be0abb435ddb84e5
prasanna <lakshmiprasanna121212@gmail.com>
2025-12-09 13:56:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
-$conn = new mysqli("localhost", "root", "", "library_db");
+require_once __DIR__ . "/config/db_config.php";
+
+$conn = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
