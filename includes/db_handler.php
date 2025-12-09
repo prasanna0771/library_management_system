073926b1d371946e30d36124f0ceeb29d2222fdc
prasanna <lakshmiprasanna121212@gmail.com>
2025-12-09 13:53:55 +0530

    Update db_handler.php

--- includes/db_handler.php
+++ includes/db_handler.php
@@ -3,7 +3,14 @@ class Connection {
     protected $conn;
 
     public function __construct() {
-        $this->conn = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+        require_once __DIR__ . "/config/db_config.php";
+
+$conn = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
